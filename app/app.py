92707ec30a8e64feae68d96fe307c63fc733a233
chlv <lc1923@live.cn>
2018-04-24 20:25:01 +0800

    first add restful

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from flask_restful import reqparse, abort, Api, Resource
 
 
 app = Flask(__name__)
+api = Api(app)
+
+
+class RecordDriverConfig(Resource):
+    def get(self, config):
+        pass
 
 
 
@@ -10,6 +17,7 @@ def hello_world():
     return "Hello World!"
 
 
+api.add_resource(RecordDriverConfig, "/uploadconfig/<config>")
 if __name__ == "__main__":
     app.debug = True
     app.run()
